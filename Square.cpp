ae56b64195865ee5d20d1ff2e945ad4ef9791c9f
white0nrice <bryanjoe421@gmail.com>
2017-03-09 04:03:49 -0800

    Added comments

--- Square.cpp
+++ Square.cpp
@@ -3,12 +3,13 @@
 #include <cmath>
 using namespace std;
 
+//Creates Square object with one side since all sides are equal
 Square::Square(double s) {
 	side = s;
 	setArea(0.0);
 	setPerimeter(0.0);
 }
-
+//calculates area and perimeter of square
 void Square::calculate() {
 	setArea(side * side);
 	setPerimeter(side * S);
